f3bf1eb0dff74686051ea801a33c5279edbda404
Edgardo <edgardoolivares@cpapaseo.com>
2026-01-23 16:28:38 -0600

    Usando Image

--- primerospasos/app/components/home/Hero/Hero.tsx
+++ primerospasos/app/components/home/Hero/Hero.tsx
@@ -1,9 +1,9 @@
-import style from './Hero.module.css'
+import styles from './Hero.module.css'
 
 export const Hero = () => {
   
   return (
-    <section className={style.Hero}>
+    <section className={styles.Hero}>
       <h1>Future world</h1>
       <h2>Empowering Your Tomorrow, Today!</h2>
     </section>
--- primerospasos/app/layout.tsx
+++ primerospasos/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Header } from "./components/shared/Header/Header";  
 import { Footer } from "./components/shared/Footer";
+import './sass/globals.sass'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
